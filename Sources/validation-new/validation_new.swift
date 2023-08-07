3541c6ca6e6ddd7c420ab4b0e78aa7c0324b8257
Manali <46921544+manalirs@users.noreply.github.com>
2023-08-08 01:28:34 +0530

    Update validation_new.swift

--- Sources/validation-new/validation_new.swift
+++ Sources/validation-new/validation_new.swift
@@ -65,10 +65,11 @@ public class validation_new {
   
 
  public  func limitUserName(name1: String)-> String {
-   
+   name = name1
       if name.count > 4 {
                                     name = String(name.prefix(4))
                                 }
+    return name
    }
     
     

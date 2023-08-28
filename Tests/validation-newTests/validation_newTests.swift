e199b7791a32bcdc2a24a1d8ede698485b579ff2
Manali <46921544+manalirs@users.noreply.github.com>
2023-08-28 11:31:25 +0530

    Update validation_newTests.swift

--- Tests/validation-newTests/validation_newTests.swift
+++ Tests/validation-newTests/validation_newTests.swift
@@ -4,6 +4,14 @@ import XCTest
 @testable import validation_new
 
 final class validation_newTests: XCTestCase {
+
+        func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(DummyPackage().text, "Hello, World!")
+    }
+
     var calculator: validation_new!
 
     override func setUp() {
